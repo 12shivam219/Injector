9e06472b1b29e9a95d2f2ae52b2b042efe82f91b
GitHub User <user@example.com>
2025-09-20 10:00:54 +0530

    Remove two test files

--- temp_check_columns.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy import create_engine, text
-os.environ['DATABASE_URL']='postgresql://neondb_owner:npg_dQ6JOVN9AGSD@ep-raspy-feather-a8zo9edv-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
-engine = create_engine(os.environ['DATABASE_URL'])
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT column_name, data_type FROM information_schema.columns WHERE table_name='user_sessions' ORDER BY ordinal_position"))
-    for r in res:
-        print(r)
-engine.dispose()
